45d5909736b03327087f7a1ffe653190abb53b8d
Yeoksayeou <kaebyeokarchive@yahoo.com>
2025-04-23 16:17:31 +0100

    updated index

--- shenbao/data/overview.js
+++ shenbao/data/overview.js
@@ -1,14 +1,14 @@
 // Site overview information
 window.SITE_INFO = {
     title: "申報",
-    version: "1.0.0",
+    version: "1.2.0",
     sourceLink: "https://github.com/moss-on-stone/shenbao-txt",
     sourceName: "shenbao repository",
-    lastUpdated: "April 8, 2025",
+    lastUpdated: "April 23, 2025",
     introductionText: `<p>The text and translation of these issues of the <em>Shenbao</em> (so far) includes issues published in 
     Shanghai (上海) and, for a few months in 1939, also Hong Kong (香港). Eventual Japanese control over the newspaper in Shanghai from December, 1941 can be seen from a sudden and radical shift in war reporting. 
     Keep in mind that this newspaper is sometimes missing issues. Sometimes this may be due to issues that weren't ever published, but may also be issues 
-    that are not in the repository with transcribed articles this used. There may also have been problems with the translation.
+    that are not in the repository with transcribed articles this used.</p><p>There may also have been problems with the translation. 
     Besides missing issues, keep an eye out for missing articles in the translation of 
     some issues due to errors in the translation process when breaking up large issues. More years (including various locations the newspaper was published in 1938) may be added later.</p>
     <p>See also <a href="today.html">Today in the Japanese Occupation of China</a></p>`
