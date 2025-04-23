b5f3813180c63ed79591dae19aef359794b4af14
Yeoksayeou <kaebyeokarchive@yahoo.com>
2025-04-23 17:06:38 +0100

    Added anchors

--- shenbao/data/overview.js
+++ shenbao/data/overview.js
@@ -8,8 +8,8 @@ window.SITE_INFO = {
     introductionText: `<p>The text and translation of these issues of the <em>Shenbao</em> (so far) includes issues published in 
     Shanghai (上海) and, for a few months in 1939, also Hong Kong (香港). Eventual Japanese control over the newspaper in Shanghai from December, 1941 can be seen from a sudden and radical shift in war reporting. 
     Keep in mind that this newspaper is sometimes missing issues. Sometimes this may be due to issues that weren't ever published, but may also be issues 
-    that are not in the repository with transcribed articles this used.</p><p>There may also have been problems with the translation. 
+    that are not in the repository with transcribed articles this used.</p><p>There may also have been problems with the automatic translation. 
     Besides missing issues, keep an eye out for missing articles in the translation of 
-    some issues due to errors in the translation process when breaking up large issues. More years (including various locations the newspaper was published in 1938) may be added later.</p>
+    some issues due to errors in the translation process when breaking up large issues into smaller pieces. Comparing the small article numbers after article headlines in the translation and original can help alert you to missing content. More years (including various locations the newspaper was published in 1938) may be added later.</p>
     <p>See also <a href="today.html">Today in the Shenbao</a></p>`
 };
